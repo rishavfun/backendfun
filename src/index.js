40fbe6b8e3a961b535d01eaf6c8b32922b16537c
rishavfun <rishav9097@gmail.com>
2022-08-14 14:30:28 +0530

    mongo_intro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', route);
 
+mongoose.connect("mongodb+srv://rishav:cNUVlhgpdlf8pXMx@cluster0.zvf3loz.mongodb.net/rishav_90?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch(err => console.log(err))
+
+app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
